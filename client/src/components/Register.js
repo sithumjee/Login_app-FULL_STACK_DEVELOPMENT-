fc3a0e6d11d632dc79619a38d76a7ac9045adad3
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2023-12-29 19:39:06 +0530

    finished register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Username.css";
 import { Link } from "react-router-dom";
 import avatar from "../assets/avatar.svg";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { accountValidate } from "../helper/validate";
-// import { passwordValidate } from "../helper/validate";
+import { registerValidate } from "../helper/validate";
+import convertBase64 from "../helper/convert";
 
 export default function Register() {
+  const [file, setFile] = useState();
+
   const formik = useFormik({
     initialValues: {
       username: "",
       email: "",
       password: "",
     },
-    validate: accountValidate,
+    validate: registerValidate,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
+      values = await Object.assign(values, { profile: file || "" });
       console.log(values);
     },
   });
+
+  /**Formik doesnt support file formats */
+
+  const onUpload = async (e) => {
+    const base64 = await convertBase64(e.target.files[0]);
+    setFile(base64);
+  };
 
   return (
     <>
@@ -44,11 +55,11 @@ export default function Register() {
           <p style={{ paddingLeft: "30px" }}>
             Explore more by registering with us
           </p>
-          <label htmlFor="profile">
-            <img src={avatar} alt="avatar" />
+          <label className="profile_img" htmlFor="profile">
+            <img src={file || avatar} alt="avatar" />
           </label>
 
-          <input type="file" id="profile" name="profile" />
+          <input onChange={onUpload} type="file" id="profile" name="profile" />
           {/**login data check */}
 
           <form onSubmit={formik.handleSubmit}>
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -18,10 +18,18 @@ export async function passwordValidate(values) {
 
 export async function resetValidate(values) {
   const error = passwordVerify({}, values);
-
   if (values.password !== values.confPs) {
     error.confPs = toast.error("Password does not match");
   }
+  return error;
+}
+
+/**validate register */
+
+export async function registerValidate(values) {
+  const error = accountVerify({}, values);
+  passwordVerify(error, values);
+  emailVerify(error, values);
   return error;
 }
 
@@ -64,6 +72,22 @@ function passwordVerify(error = {}, values) {
     error.password = toast.error("Password is less than 6 characters");
   } else if (!specialChars.test(values.password)) {
     error.password = toast.error("Password must contain a special character");
+  }
+
+  return error;
+}
+
+/**================================================= */
+
+function emailVerify(error = {}, values) {
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  if (!values.email) {
+    error.email = toast.error("Email is required");
+  } else if (values.email.includes(" ")) {
+    error.email = toast.error("Email is invalid");
+  } else if (!emailRegex.test(values.email)) {
+    error.email = toast.error("Email is invalid");
   }
 
   return error;
